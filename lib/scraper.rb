318c56a860d229f1f1ce3515f82e504a82d387fb
sarah <s.j.condon01@gmail.com>
2019-10-23 23:05:20 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,12 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    html(open index_url)
+    list = Nokogiri::HTML(html)
+    names = list.css(".student-name")
+    names_array = []
+    names.each do |item|
+      names_array << item.text
   end
 
   def self.scrape_profile_page(profile_url)
